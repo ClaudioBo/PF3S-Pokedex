44d20161828c659efb8fb6cf4ce89378b6ad5a46
Kledioz <claudiobo1122@gmail.com>
2019-12-11 17:44:49 -0700

    Skrt

--- Pokedex/src/main/java/mx/itson/pokemon/entidades/RespuestaCaramelos.java
+++ Pokedex/src/main/java/mx/itson/pokemon/entidades/RespuestaCaramelos.java
@@ -4,6 +4,10 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import mx.itson.pokemon.transporte.PokemonCaramelosDTO;
 
+/**
+ * 
+ * Recibe las lista de los caramelos que los pokemones necesitan para evolucionar.
+ */
 public class RespuestaCaramelos {
 
     @SerializedName("12")
@@ -33,74 +37,199 @@ public class RespuestaCaramelos {
     @SerializedName("400")
     private ArrayList<PokemonCaramelosDTO> cuatrocientosCaramelos;
 
+    /**
+     * Obtiene un lista de la api de los caramelos que los 
+     * pokemones que ocupan 12 caramelos para evolucionar.
+     
+     * @return Un lista de la api de los caramelos que los 
+     * pokemones que ocupan 12 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getDoceCaramelos() {
         return doceCaramelos;
     }
 
+    /**
+     * Cambia la lista de la api de los caramelos que los
+     * pokemones que ocupan 12 caramelos para evolucionar.
+     * 
+     * @param doceCaramelos La lista de la api de los caramelos que los
+     * pokemones que ocupan 12 caramelos para evolucionar.
+     */
     public void setDoceCaramelos(ArrayList<PokemonCaramelosDTO> doceCaramelos) {
         this.doceCaramelos = doceCaramelos;
     }
 
+    /**
+     * Obtiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 22 caramelos para evolucionar.
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 22 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getVeinteydosCaramelos() {
         return veinteydosCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 22 caramelos para evolucionar.
+     * 
+     * @param veinteydosCaramelos Un lista de la api de los 
+     * caramelos que los pokemones que ocupan 22 caramelos para evolucionar.
+     */
     public void setVeinteydosCaramelos(ArrayList<PokemonCaramelosDTO> veinteydosCaramelos) {
         this.veinteydosCaramelos = veinteydosCaramelos;
     }
 
+    /**
+     * Obtiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 25 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 25 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getVeinteycincoCaramelos() {
         return veinteycincoCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 25 caramelos para evolucionar.
+     * 
+     * @param veinteycincoCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 25 caramelos para evolucionar.
+     */
     public void setVeinteycincoCaramelos(ArrayList<PokemonCaramelosDTO> veinteycincoCaramelos) {
         this.veinteycincoCaramelos = veinteycincoCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 45 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 45 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getCuarentaycincoCaramelos() {
         return cuarentaycincoCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 45 caramelos para evolucionar.
+     * 
+     * @param cuarentaycincoCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 45 caramelos para evolucionar.
+     */
     public void setCuarentaycincoCaramelos(ArrayList<PokemonCaramelosDTO> cuarentaycincoCaramelos) {
         this.cuarentaycincoCaramelos = cuarentaycincoCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 50 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 50 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getCincuentaCaramelos() {
         return cincuentaCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 50 caramelos para evolucionar.
+     * 
+     * @param cincuentaCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 50 caramelos para evolucionar.
+     */
     public void setCincuentaCaramelos(ArrayList<PokemonCaramelosDTO> cincuentaCaramelos) {
         this.cincuentaCaramelos = cincuentaCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 90 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 90 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getNoventaCaramelos() {
         return noventaCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 90 caramelos para evolucionar.
+     * 
+     * @param noventaCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 90 caramelos para evolucionar.
+     */
     public void setNoventaCaramelos(ArrayList<PokemonCaramelosDTO> noventaCaramelos) {
         this.noventaCaramelos = noventaCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 100 caramelos para evolucionar.
+     * 
+     * @return Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 100 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getCienCaramelos() {
         return cienCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 100 caramelos para evolucionar
+     * 
+     * @param cienCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 100 caramelos para evolucionar.
+     */
     public void setCienCaramelos(ArrayList<PokemonCaramelosDTO> cienCaramelos) {
         this.cienCaramelos = cienCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 360 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 360 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getTrescientossesentaCaramelos() {
         return trescientossesentaCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 360 caramelos para evolucionar
+     * 
+     * @param trescientossesentaCaramelos Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 360 caramelos para evolucionar
+     */
     public void setTrescientossesentaCaramelos(ArrayList<PokemonCaramelosDTO> trescientossesentaCaramelos) {
         this.trescientossesentaCaramelos = trescientossesentaCaramelos;
     }
 
+    /**
+     * Obitiene una lista de la api de los caramelos que los
+     * pokemones que ocupan 400 caramelos para evolucionar.
+     * 
+     * @return Una lista de la api de los caramelos que los
+     * pokemones que ocupan 400 caramelos para evolucionar.
+     */
     public ArrayList<PokemonCaramelosDTO> getCuatrocientosCaramelos() {
         return cuatrocientosCaramelos;
     }
 
+    /**
+     * Cambia una lista de la api de los caramelos que los
+     * pokemones que ocupan 400 caramelos para evolucionar.
+     * 
+     * @param cuatrocientosCaramelos Una lista de la api de los caramelos que los
+     * pokemones que ocupan 400 caramelos para evolucionar.
+     */
     public void setCuatrocientosCaramelos(ArrayList<PokemonCaramelosDTO> cuatrocientosCaramelos) {
         this.cuatrocientosCaramelos = cuatrocientosCaramelos;
     }
